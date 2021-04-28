430b34ca8ccda6598f5d2b0f878e8246fb4492c3
aakash2809 <aakashrajak289@gmail.com>
2021-04-28 19:07:32 +0530

    refactor : add route for forgot password

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,34 @@
-
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelleoWorld from '@/components/HelloWorld';
-import Login from '@/pages/Login';
-import Registration from '@/pages/Register';
 import RegistrationOrLogin from '@/pages/RegistrationOrLogin';
 import DashBoard from '@/pages/DashBoard';
+import ForgotPassword from '@/pages/ForgotPassword';
+import ForgotPasswordForm from '@/components/ForgotPasswordForm';
 
 Vue.use(Router);
 
 export default new Router({
-	routes: [{
-		path: '/helleoWorld',
-		name: HelleoWorld,
-		component: HelleoWorld
-	},
-	{
-		path: '/login',
-		name: Login,
-		component: Login
-	}, {
-		path: '/register',
-		name: Registration,
-		component: Registration
-	},
-	{
-		path: '/registrationOrLogin',
-		name: RegistrationOrLogin,
-		component: RegistrationOrLogin
-	},
-	{
-		path: '/dashBoard',
-		name: DashBoard,
-		component: DashBoard
-	}
+	routes: [
+		{
+			path: '/registrationOrLogin',
+			name: RegistrationOrLogin,
+			component: RegistrationOrLogin
+		},
+		{
+			path: '/dashBoard',
+			name: DashBoard,
+			component: DashBoard
+		},
+		{
+			path: '/forgotPassword',
+			name: ForgotPassword,
+			component: ForgotPassword
+		},
+		{
+			path: '/forgotPasswordForm',
+			name: ForgotPasswordForm,
+			component: ForgotPasswordForm
+		}
 	],
 	mode: 'history'
 });
